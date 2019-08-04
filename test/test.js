f2687fc4ae473c719a539b9e137dc763543e6380
HjAkeru <window.akeru@gmail.com>
2019-08-04 04:46:25 +0000

    予定の関連項目削除テストの実装

--- test/test.js
+++ test/test.js
@@ -268,20 +268,24 @@ describe('/schedules/:scheduleId?delete=1', () => {
             const p1 = Comment.findAll({
               where: { scheduleId: scheduleId }
             }).then((comments) => {
-              // TODO テストを実装
+              // コメント削除テスト
+              assert.equal(comments.length,0);
             });
             const p2 = Availability.findAll({
               where: { scheduleId: scheduleId }
             }).then((availabilities) => {
-              // TODO テストを実装
+              // 出欠削除テスト
+              assert.equal(availabilities.length, 0);
             });
             const p3 = Candidate.findAll({
               where: { scheduleId: scheduleId }
             }).then((candidates) => {
-              // TODO テストを実装
+              // 候補削除テスト
+              assert.equal(candidates.length, 0);
             });
             const p4 = Schedule.findByPk(scheduleId).then((schedule) => {
-              // TODO テストを実装
+              // 予定削除テスト
+              assert.equal(!schedule, true);
             });
             Promise.all([p1, p2, p3, p4]).then(() => {
               if (err) return done(err);
