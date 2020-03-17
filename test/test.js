abfae66979c393bc3d8201e95efa0979842bd182
piyo2019 <hakobe2001@yahoo.co.jp>
2020-03-17 20:39:23 +0900

    すべての情報が削除できるかのテストを実装しました

--- test/test.js
+++ test/test.js
@@ -75,7 +75,7 @@ describe('/schedules', () => {
             .expect(/テスト候補2/)
             .expect(/テスト候補3/)
             .expect(200)
-            .end((err, res) => { deleteScheduleAggregate(createdSchedulePath.split('/schedules/')[1], done, err); });
+            .end((err, res) => { deleteScheduleAggregate(createdSchedulePath.split('/schedules/')[1], done, err);});
         });
     });
   });
@@ -185,13 +185,12 @@ describe('/schedules/:scheduleId?edit=1', () => {
             .post(`/schedules/${scheduleId}?edit=1`)
             .send({ scheduleName: 'テスト更新予定2', memo: 'テスト更新メモ2', candidates: 'テスト更新候補2' })
             .end((err, res) => {
-              Schedule.findByPk(scheduleId).then((s) => {
+              Schedule.findById(scheduleId).then((s) => {
                 assert.equal(s.scheduleName, 'テスト更新予定2');
                 assert.equal(s.memo, 'テスト更新メモ2');
               });
               Candidate.findAll({
-                where: { scheduleId: scheduleId },
-                order: [['"candidateId"', 'ASC']]
+                where: { scheduleId: scheduleId }
               }).then((candidates) => {
                 assert.equal(candidates.length, 2);
                 assert.equal(candidates[0].candidateName, 'テスト更新候補1');
@@ -268,20 +267,20 @@ describe('/schedules/:scheduleId?delete=1', () => {
             const p1 = Comment.findAll({
               where: { scheduleId: scheduleId }
             }).then((comments) => {
-              // TODO テストを実装
+              assert.equal(comments.length, 0);
             });
             const p2 = Availability.findAll({
               where: { scheduleId: scheduleId }
             }).then((availabilities) => {
-              // TODO テストを実装
+              assert.equal(availabilities.length, 0);
             });
             const p3 = Candidate.findAll({
               where: { scheduleId: scheduleId }
             }).then((candidates) => {
-              // TODO テストを実装
+              assert.equal(candidates.length, 0);
             });
-            const p4 = Schedule.findByPk(scheduleId).then((schedule) => {
-              // TODO テストを実装
+            const p4 = Schedule.findById(scheduleId).then((schedule) => {
+              assert.equal(!schedule, true);
             });
             Promise.all([p1, p2, p3, p4]).then(() => {
               if (err) return done(err);
