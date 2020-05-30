3fa9b8f2de65228cb14200d19f3ea04de860cfe0
ty4r <ty4rsig@gmail.com>
2020-05-31 00:01:40 +0900

    4021

--- test/test.js
+++ test/test.js
@@ -302,3 +302,103 @@ describe('/schedules/:scheduleId?delete=1', () => {
     });
   });
 });
+
+describe('/schedules/:scheduleId?delete=1', () => {
+  beforeAll(() => {
+    passportStub.install(app);
+    passportStub.login({ id: 0, username: 'testuser' });
+  });
+
+  afterAll(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  test('予定に関連する全ての情報が削除できる', done => {
+    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+      request(app)
+        .post('/schedules')
+        .send({
+          scheduleName: 'テスト更新予定1',
+          memo: 'テスト更新メモ1',
+          candidates: 'テスト更新候補1'
+        })
+        .end((err, res) => {
+          const createdSchedulePath = res.headers.location;
+          const scheduleId = createdSchedulePath.split('/schedules/')[1];
+
+          // 出欠作成
+          const promiseAvailability = Candidate.findOne({
+            where: { scheduleId: scheduleId }
+          }).then(candidate => {
+            return new Promise(resolve => {
+              const userId = 0;
+              request(app)
+                .post(
+                  `/schedules/${scheduleId}/users/${userId}/candidates/${candidate.candidateId}`
+                )
+                .send({ availability: 2 }) // 出席に更新
+                .end((err, res) => {
+                  if (err) done(err);
+                  resolve();
+                });
+            });
+          });
+
+          // コメント作成
+          const promiseComment = new Promise(resolve => {
+            const userId = 0;
+            request(app)
+              .post(`/schedules/${scheduleId}/users/${userId}/comments`)
+              .send({ comment: 'testcomment' })
+              .expect('{"status":"OK","comment":"testcomment"}')
+              .end((err, res) => {
+                if (err) done(err);
+                resolve();
+              });
+          });
+
+          // 削除
+          const promiseDeleted = Promise.all([
+            promiseAvailability,
+            promiseComment
+          ]).then(() => {
+            return new Promise(resolve => {
+              request(app)
+                .post(`/schedules/${scheduleId}?delete=1`)
+                .end((err, res) => {
+                  if (err) done(err);
+                  resolve();
+                });
+            });
+          });
+
+          // テスト
+          promiseDeleted.then(() => {
+            const p1 = Comment.findAll({
+              where: { scheduleId: scheduleId }
+            }).then(comments => {
+              assert.equal(comments.length, 0); //コメント
+            });
+            const p2 = Availability.findAll({
+              where: { scheduleId: scheduleId }
+            }).then(availabilities => {
+              assert.equal(availabilities.length, 0); //出欠
+            });
+            const p3 = Candidate.findAll({
+              where: { scheduleId: scheduleId }
+            }).then(candidates => {
+              assert.equal(candidates.length, 0); //候補
+            });
+            const p4 = Schedule.findByPk(scheduleId).then(schedule => {
+              assert.equal(!schedule, true); //予定
+            });
+            Promise.all([p1, p2, p3, p4]).then(() => {
+              if (err) return done(err);
+              done();
+            });
+          });
+        });
+    });
+  });
+});
