0f5733128168d95e5fd145f432b6709bf118289d
sho01072020 <takagi.s3637@gmail.com>
2021-01-25 11:19:57 +0900

    削除に関するテストを修正

--- test/test.js
+++ test/test.js
@@ -278,6 +278,7 @@ describe('/schedules/:scheduleId?delete=1', () => {
             const p1 = Comment.findAll({
               where: { scheduleId: scheduleId }
             }).then((comments) => {
+              // TODO テストを実装
               assert.strictEqual(comments.length, 0);
             });
             const p2 = Availability.findAll({
