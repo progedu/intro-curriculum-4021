d517dc90990a4a041075d5a3d460c95491280f19
Hidenobu Satsumi <stm323stm@gmail.com>
2021-04-15 22:08:53 +0900

    全てデータベース上から削除されていることをテストする実装

--- test/test.js
+++ test/test.js
@@ -225,11 +225,15 @@ describe('/schedules/:scheduleId?delete=1', () => {
     passportStub.uninstall(app);
   });
 
-  test('予定に関連する全ての情報が削除できる', (done) => {
+  test('予定に関連する全ての情報が削除できる', done => {
     User.upsert({ userId: 0, username: 'testuser' }).then(() => {
       request(app)
         .post('/schedules')
-        .send({ scheduleName: 'テスト更新予定1', memo: 'テスト更新メモ1', candidates: 'テスト更新候補1' })
+        .send({
+          scheduleName: 'テスト更新予定1',
+          memo: 'テスト更新メモ1',
+          candidates: 'テスト更新候補1'
+        })
         .end((err, res) => {
           const createdSchedulePath = res.headers.location;
           const scheduleId = createdSchedulePath.split('/schedules/')[1];
@@ -237,10 +241,13 @@ describe('/schedules/:scheduleId?delete=1', () => {
           // 出欠作成
           const promiseAvailability = Candidate.findOne({
             where: { scheduleId: scheduleId }
-          }).then((candidate) => {
-            return new Promise((resolve) => {
+          }).then(candidate => {
+            return new Promise(resolve => {
+              const userId = 0;
               request(app)
-                .post(`/schedules/${scheduleId}/users/${0}/candidates/${candidate.candidateId}`)
+                .post(
+                  `/schedules/${scheduleId}/users/${userId}/candidates/${candidate.candidateId}`
+                )
                 .send({ availability: 2 }) // 出席に更新
                 .end((err, res) => {
                   if (err) done(err);
@@ -250,9 +257,10 @@ describe('/schedules/:scheduleId?delete=1', () => {
           });
 
           // コメント作成
-          const promiseComment = new Promise((resolve) => {
+          const promiseComment = new Promise(resolve => {
+            const userId = 0;
             request(app)
-              .post(`/schedules/${scheduleId}/users/${0}/comments`)
+              .post(`/schedules/${scheduleId}/users/${userId}/comments`)
               .send({ comment: 'testcomment' })
               .expect('{"status":"OK","comment":"testcomment"}')
               .end((err, res) => {
@@ -262,8 +270,11 @@ describe('/schedules/:scheduleId?delete=1', () => {
           });
 
           // 削除
-          const promiseDeleted = Promise.all([promiseAvailability, promiseComment]).then(() => {
-            return new Promise((resolve) => {
+          const promiseDeleted = Promise.all([
+            promiseAvailability,
+            promiseComment
+          ]).then(() => {
+            return new Promise(resolve => {
               request(app)
                 .post(`/schedules/${scheduleId}?delete=1`)
                 .end((err, res) => {
@@ -277,21 +288,25 @@ describe('/schedules/:scheduleId?delete=1', () => {
           promiseDeleted.then(() => {
             const p1 = Comment.findAll({
               where: { scheduleId: scheduleId }
-            }).then((comments) => {
+            }).then(comments => {
               // TODO テストを実装
+              assert.strictEqual(comments.length, 0);
             });
             const p2 = Availability.findAll({
               where: { scheduleId: scheduleId }
-            }).then((availabilities) => {
+            }).then(availabilities => {
               // TODO テストを実装
+              assert.strictEqual(availabilities.length, 0);
             });
             const p3 = Candidate.findAll({
               where: { scheduleId: scheduleId }
-            }).then((candidates) => {
+            }).then(candidates => {
               // TODO テストを実装
+              assert.strictEqual(candidates.length, 0);
             });
-            const p4 = Schedule.findByPk(scheduleId).then((schedule) => {
+            const p4 = Schedule.findByPk(scheduleId).then(schedule => {
               // TODO テストを実装
+              assert.strictEqual(!schedule, true);
             });
             Promise.all([p1, p2, p3, p4]).then(() => {
               if (err) return done(err);
